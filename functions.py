1980c02eaa94ef3430833e261910919c45a8910a
madeleineglrt <gilbert.madeleine@ndbellegarde.net>
2026-04-28 11:50:46 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,6 +2,16 @@ def saisir_lettre():
     lettre = str(input("Quelle lettre voulez-vous proposer?"))
     return lettre
 
+def chercher_lettre(lettre,mot):
+    ''' la fonction chercher_lettre prend en paramètre la lettre
+    choisie par l'utilisateur précédemment et le mot concerné dans le jeu
+    Elle renvoie un tuple contenant les indices des occurences de la lettre
+    dans le mot
+    '''
+    for indice in mot:
+        if mot[indice] == lettre
+
+
 def choix_mot(list):
     '''Cette fonction choisit un mot au hasard dans une liste de mots'''
     import random
