4d4e1591335c0042534453a1369a292cff9a47e1
Mart1 <165647245+Mart1Potato@users.noreply.github.com>
2026-04-28 11:48:48 +0200

    Add word selection and hiding functions
    Added functions to choose and hide a word from a list.

--- functions.py
+++ functions.py
@@ -2,3 +2,15 @@ def saisir_lettre():
     lettre = str(input("Quelle lettre voulez-vous proposer?"))
     return lettre
 
+def choix_mot(list):
+    '''Cette fonction choisit un mot au hasard dans une liste de mots'''
+    import random
+    mot = random.choice(list)
+    return mot
+
+def cacher_mot(mot):
+    '''Cette fonction cache le mot choisi en remplaçant chaque lettre par un astérisque'''
+    mot_cache = []
+    for car in mot:
+        mot_cache.append("*")
+    return mot_cache
